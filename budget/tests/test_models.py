fd0e5707dc8562f7397986b36b1093b97fce8967
mko <mko@startext.ru>
2020-09-06 18:49:14 +0700

    empty tests for models Aim, Budget, Expense, Income added

---
+++ budget/tests/test_models.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from budget.models.aim import Aim
+from budget.models.budget import Budget
+from budget.models.expense import Expense
+from budget.models.income import Income
+
+
+class AimModelTest(TestCase):
+    pass
+
+
+class BudgetModelTest(TestCase):
+    pass
+
+
+class ExpenseModelTest(TestCase):
+    pass
+
+
+class IncomeModelTest(TestCase):
+    pass
