0cdb9800129ccd256ddc907e6558d141b2c47905
MKomaltilov <marat.komaltilov@icloud.com>
2020-09-13 11:57:18 +0700

    Income model added

--- budget/models/Income.py
+++ budget/models/Income.py
@@ -1,5 +1,11 @@
+import uuid
 from django.db import models
+from budget.models.Budget import Budget
 
 
 class Income(models.Model):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    value = models.PositiveBigIntegerField()
+    budget = models.ForeignKey(to=Budget, related_name='budgets', on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
