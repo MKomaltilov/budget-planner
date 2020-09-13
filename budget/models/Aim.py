ef8c7cbf7c66638b75abf79c97621fa41f91aed4
MKomaltilov <marat.komaltilov@icloud.com>
2020-09-13 11:51:14 +0700

    Aim model added

---
+++ budget/models/Aim.py
@@ -0,0 +1,18 @@
+import uuid
+from django.db import models
+from budget.models.Budget import Budget
+
+
+class Aim(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    value = models.PositiveBigIntegerField()
+    budget = models.ForeignKey(to=Budget, related_name='aims', on_delete=models.CASCADE)
+    end_date = models.DateField()
+
+
+    # id (uuid)
+    # name (charfield, max 255)
+    # value (positive integer)
+    # budget (fk->Budget)
+    # end_date (datefield)
