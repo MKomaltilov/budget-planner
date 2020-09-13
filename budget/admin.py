c6d2c6b0f74717910299ee83632579b84106f906
MKomaltilov <marat.komaltilov@icloud.com>
2020-09-13 12:33:59 +0700

    test for Aim model added

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from budget.models.Aim import Aim
+from budget.models.Budget import Budget
+from budget.models.Income import Income
+from budget.models.Expense import Expense
 
-# Register your models here.
+
+admin.site.register(Aim)
+admin.site.register(Budget)
+admin.site.register(Income)
+admin.site.register(Expense)
--- budget/tests/test_models.py
+++ budget/tests/test_models.py
@@ -1,12 +1,36 @@
+from datetime import date
+
 from django.test import TestCase
-from budget.models.aim import Aim
-from budget.models.budget import Budget
-from budget.models.expense import Expense
-from budget.models.income import Income
+
+from budget.models.Aim import Aim
+from budget.models.Budget import Budget
+from budget.models.Expense import Expense
+from budget.models.Income import Income
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class AimModelTest(TestCase):
-    pass
+    def test_aim_can_be_created(self):
+        user = User.objects.create(username='admin', password='123')
+        budget = Budget.objects.create(name='main', owner=user)
+        date_now = date.today()
+
+        Aim.objects.create(
+            name='Buy a house',
+            value=4_000_000,
+            budget=budget,
+            end_date=date_now
+        )
+
+        aim = Aim.objects.first()
+
+        self.assertEqual(aim.name, 'Buy a house')
+        self.assertEqual(aim.value, 4_000_000)
+        self.assertEqual(aim.budget, budget)
+        self.assertEqual(aim.end_date, date_now)
 
 
 class BudgetModelTest(TestCase):
